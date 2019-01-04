a8aa1ee1d84680e415beb7ffe6052e20f7eac9a7
devill <devillmeister@gmail.com>
2019-01-04 20:57:20 +0100

    1205_2

--- 1205_1.py
+++ 1205_1.py
@@ -1,30 +1,27 @@
 
 def reduced(string):
     rs = ""
-    highlights = ""
     i = 0
     while i < len(string) - 1:
         if string[i] != string[i + 1].swapcase():
-            highlights += string[i]
             rs += string[i]
             i+=1
         else:
-            highlights += '\033[1m' + string[i] + string[i+1] + '\033[0m'
             i+=2 
 
     if i < len(string):
-        highlights += string[i]
         rs += string[i]
 
-    if rs == string:
-        return rs
-    else:
-        print highlights
-        print rs
-        return reduced(rs)
-
+    return rs
 
 with open("1205.txt") as f:
     content = f.readlines()[0].strip()
 
-print len(reduced(content))
+n = reduced(content)
+
+while len(n) != len(content):
+    content = n
+    n = reduced(content)
+
+
+print len(n)
